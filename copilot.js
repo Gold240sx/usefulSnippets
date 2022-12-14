dbc72adb948b1623d4694a903a270a7cd3bdd54d
Gold240sx <240designworks@gmail.com>
2022-12-14 17:01:47 -0600

    Name Formatter

--- copilot.js
+++ copilot.js
@@ -4,14 +4,20 @@ var regex_all_two_letter_words = /(\b\w{2}\b)/g;
 var regex_all_one_letter_words = /(\b\w{1}\b)/g;
 var regex_isolate_2lett = /\b([a-zA-Z])([a-zA-Z])\b/g;
 var regexCapitalize = /[A-Z]+/g;
+var regex_lowercase_last_letter = /(\w)(\W*)$/;
+var regex_is_letter = /^[a-z]/i;
+var regex_is_letter_or_space = /^[a-zA-Z\s]+$/i;
 
 var testString = [
 	"j handcock",
 	"je handcock",
+	"j.e. handcock",
 	"jim handcock",
 	"jim j handcock",
 	"jim je handcock,",
 	"jim jer handcock",
+	"jo jenny handcock",
+	"jo jim le handcock",
 	"j handcock",
 	"j. e, handcock",
 	"j. er handcock jr",
@@ -20,29 +26,26 @@ var testString = [
 ];
 
 testString.forEach((str) => {
-	var firstStr = str.replaceAll(".", "").replaceAll(",", "");
+	//uppercase all 2 letter words. always lowercase the last letter
+	var initStr = str
+		.split(" ")
+		.map((i) => (i.replaceAll(/[^a-z]/gi, "").length === 2 ? i.toUpperCase() : i))
+		.join(" ")
+		.replace(regex_lowercase_last_letter, function (match, capture1, capture2) {
+			return capture1.toLowerCase() + capture2;
+		});
+	//remove all periods and commas
+	var firstStr = initStr.replaceAll(".", "").replaceAll(",", "");
+	//add periods to all 2 letter words
 	var secndStr = firstStr.replace(regex_all_two_letter_words, "$1.");
-	var thirdStr = secndStr.replace(regex_all_one_letter_words, "$1.", (letter) => letter.toUpperCase());
+	//add periods to all 1 letter words
+	var thirdStr = secndStr.replace(regex_all_one_letter_words, "$1.");
+	//add periods between all 2 letter words that are not the last word
 	var fourthStr = thirdStr.replace(regex_isolate_2lett, "$1.$2");
-
-	var test = firstStr.match(regex_all_two_letter_words);
-
-	console.log(test, (word) => {
-		var word = () => {
-			if (!word == "null" || null) {
-				word = word.toUpperCase();
-				return word;
-			} else {
-				null;
-			}
-		};
-		return word;
-	});
-
+	// create exceptions for jr and sr
 	var fifthStr = fourthStr.replaceAll("j.r.", ", jr.").replaceAll("s.r.", ", sr.");
-	var sixthStr = secndStr.toUpperCase().replaceAll(regex_all_two_letter_words, "$1.");
-	// console.log(sixthStr);
 
+	//remove periods from the center of last 2 letter word
 	const lastString = (string) => {
 		if (string[string.length - 3] === ".") {
 			const partOne = string.slice(0, -3);
@@ -52,9 +55,41 @@ testString.forEach((str) => {
 		}
 		return string;
 	};
-	var finalString = lastString(fifthStr);
-	// console.log("final string", finalString);
-	const finalCap = finalString.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+	var finalStr = lastString(fifthStr);
+	//capitalize first letter of each word
+	const finalCap = finalStr.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
 
-	// console.log("finalCap", finalCap);
+	// limit all spaces to single spaces
+
+	//check if str first name of two characters includes  a period
+	const initCheck1 = str[1] === "." ? true : false;
+	const initCheck2 = str[3] === "." ? true : false;
+	const initCheckNone = initCheck1 && initCheck2 ? false : true;
+
+	const initSolve = (finalCap) => {
+		if (initCheckNone) {
+			// Assunes a 2letter first name
+			// Deletes the 1st period in the first name.
+			if (regex_is_letter_or_space.test(finalCap)) {
+				//do nothing - there are letters. or spaces and no periods
+				return finalCap;
+			} else {
+				//The user entered periods in the first name.
+				var text = finalCap.slice(0, 1) + finalCap.substr(2);
+				text = text.slice(0, 2) + text.substr(3);
+				return text;
+			}
+			finalCap = text;
+			return finalCap;
+		} else {
+			// return finalCap.replace(regex_period_first_letter, "$1$2");
+		}
+		// var txt2 = txt1.slice(0, 3) + "bar" + txt1.slice(3);
+	};
+	initSolve(finalCap);
+
+	//console.log(initCheck1, initCheck2);
+	// if not, delete the periods on the first name = str[3] + str[0]
+	// console.log(str[3]);
+	// console.log(finalCap);
 });
